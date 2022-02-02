5f7fcd0117a73b29f5b36f97641e194f3d1ce752
vicentefiorito <vicentefiorito@gmail.com>
2022-02-02 18:46:26 -0500

    worked with Styled Components

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,37 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+
+
+const FormController = styled.div`
+    margin: 0.5rem 0;
+  
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${props => (props.notValid ? 'red' : 'black')};
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => (props.notValid ? 'red' : '#ccc')};
+    background: ${props => (props.notValid ? 'ffd7d7' : 'transparent')};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+`;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -25,10 +55,10 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!enteredValid ? 'invalid' : ''}`}>
+      <FormController notValid={!enteredValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormController>
       <Button type="submit">Add Goal</Button>
     </form>
   );
